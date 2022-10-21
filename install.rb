dde174156e26076a8d95b9a288acc65834939e0e
Behrang <behrangsa@gmail.com>
2022-10-21 04:11:31 +0000

    Dotfiles 2.0

--- install.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'fileutils'
-
-# Install SSH Config
-FileUtils.mkdir_p "#{ENV['HOME']}/.ssh", :verbose => true
-FileUtils.ln_s    "#{ENV['HOME']}/dotfiles/ssh/config", "#{ENV['HOME']}/.ssh/config", :force => true, :verbose => true
