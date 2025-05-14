be31233ee2fedc127554eea0d4f3851c76114f50
Behrang Saeedzadeh <behrangsa@gmail.com>
2025-05-14 22:14:49 +0330

    Refine conky display for better fit and readability
    - Reduce font size from 15 to 10 for better display density
    - Adjust minimum and maximum width to 500px (from 949px)
    - Reorder and optimize vnstati.sh output commands
    - Remove unused weekly graph and adjust image positions
    - Optimize header display sizes

--- conky/vnstati.sh
+++ conky/vnstati.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-vnstati -i wlo1 -d 31 -c 60 -o /tmp/vnstati-d31.png
-vnstati -i wlo1 -h 24 -c 5  -o /tmp/vnstati-h24.png
-vnstati -i wlo1 -w 4 -c 5   -o /tmp/vnstati-w4.png
-vnstati -i wlo1 -m 1 -c 5   -o /tmp/vnstati-m1.png
+vnstati -i wlo1 -h 24 -c 5   -o /tmp/vnstati-h24.png
+vnstati -i wlo1 -d 31 -c 60  -o /tmp/vnstati-d31.png
+vnstati -i wlo1 -m 1  -c 240 -o /tmp/vnstati-m1.png
